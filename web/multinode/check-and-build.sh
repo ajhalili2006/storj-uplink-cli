595642c8c41d15225b124f88ce4eae5e1d5156d3
Egon Elbre <egonelbre@gmail.com>
2026-02-03 16:09:38 +0200

    go.mod: remove go.mod stubs from web/*
    Also fix the names of the scripts to be clearer.
    
    Change-Id: I3a9f9e73212c32f4bfb685aa345169d2a277f3b0

--- web/multinode/check-and-build.sh
+++ web/multinode/check-and-build.sh
@@ -5,11 +5,8 @@ set -exuo pipefail
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
 npm install --prefer-offline --no-audit --logleve verbose
-echo "module stub" > ./node_modules/go.mod # prevent Go from scanning this dir
 npm run build
 
 npm run lint-ci
 npm audit || true
 npm run test
-
-
